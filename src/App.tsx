a284e9ac89e7a71489839d64d40b499243782481
Jose Gabriel Gonzalez <gabriel.gonzalez@monks.com>
2026-03-04 18:29:42 -0500

    feat: Add Prettier configuration and apply code formatting to existing files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import './App.scss'
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -26,22 +26,16 @@ function App() {
           </nav>
         </section>
         <div className="">
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
           <p>
             Edit <code>src/App.tsx</code> and save to test HMR
           </p>
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
